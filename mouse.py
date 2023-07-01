704264451bc4d64bc05b4b5d2a7a0b44d78cb6a0
Samuel Ogogo <65853838+samosanction@users.noreply.github.com>
2023-07-01 19:29:07 +0100

    Clicking specific location, right-clicking

--- mouse.py
+++ mouse.py
@@ -5,3 +5,14 @@ position = pyautogui.position()
 pyautogui.moveTo(139, 280, duration=1)
 
 pyautogui.click(clicks=2)
+
+# Alternative code for doubleclicking
+
+pyautogui.doubleClick()
+
+# Clicking a specific location
+pyautogui.click(139, 280)
+
+# to perform a right-click on windows
+
+pyautogui.click(139, 280, button='right')
